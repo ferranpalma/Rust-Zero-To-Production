b26888761c608479b51df0bac3db7055550cb259
ferranpalma <ferranpalmac@gmail.com>
2024-05-12 17:06:42 +0200

    use settings instead of hardcoded values

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::net::TcpListener;
 
-use zero2prod::startup;
+use zero2prod::{configuration, startup};
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 8000.");
+    let configuration = configuration::get_configuration().expect("Failed to read configuration");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     startup::run(listener)?.await
 }
